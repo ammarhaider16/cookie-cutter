3fbf6941b9ce3e4df60e7faec234a466925498f6
Ammar Haider <ammar.haider@promotheanventures.net>
2025-01-23 19:51:12 -0500

    getting cookies displayed in extension

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,15 +1,14 @@
 // This script executes when the extension popup has been rendered.
-
 document.addEventListener("DOMContentLoaded", () => {
   const injectLogButton = document.getElementById("injectLogButton");
 
-  const getCookiesButton = document.getElementById("getCookiesButton");
+  const showCookiesButton = document.getElementById("showCookiesButton");
 
-  console.log("Buttons found, adding event listener.");
+  console.log("Buttons found, adding event listeners.");
 
   injectLogButton.addEventListener("click", testInjectLog);
 
-  getCookiesButton.addEventListener("click", getCookies);
+  showCookiesButton.addEventListener("click", showCookies);
 });
 
 // This function executes the callback on the tabs.Tab instance that is last active. Will not work with Chrome DevTools or "chrome://" prages are the last active tabs.
@@ -24,15 +23,12 @@ const getCurrentTab = (callback) => {
 // This is a test script injection.
 const testInjectLog = () => {
   console.log("Received log injection request.");
-
   getCurrentTab((tab) => {
     if (tab) {
-      chrome.scripting
-        .executeScript({
-          target: { tabId: tab.id },
-          func: () => console.log("This is a test injection."),
-        })
-        .then(console.log("Injected a function!"));
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => console.log("This is a test injection."),
+      });
     } else {
       console.log("No tab found!");
     }
@@ -40,6 +36,38 @@ const testInjectLog = () => {
 };
 
 // This function gets cookies that are in use in the active browser tab.
-const getCookies = () => {
+const getCookies = async () => {
   console.log("Received cookie access request.");
+  return new Promise((resolve, reject) => {
+    getCurrentTab((tab) => {
+      if (tab) {
+        chrome.scripting
+          .executeScript({
+            target: { tabId: tab.id },
+            func: () => document.cookie,
+          })
+          .then((injectionResults) => {
+            resolve(injectionResults[0].result);
+          })
+          .catch((error) => {
+            console.error("Failed to retrieve cookies:", error);
+            reject(error);
+          });
+      } else {
+        console.log("No tab found!");
+        reject(new Error("No active tab found."));
+      }
+    });
+  });
+};
+
+// This function will be the main function calling other functions to get cookies, and analyze what's going on.
+const showCookies = async () => {
+  console.log("Received cookie display request.");
+  try {
+    const cookies = await getCookies();
+    console.log("Cookies in active tab:", cookies);
+  } catch (error) {
+    console.error("Failed to display cookies:", error);
+  }
 };
